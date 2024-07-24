4fe5e0d1172ce7491a94184707d6b95234b75f7e
aammddkkzxc <ideal0427@naver.com>
2024-07-25 01:31:13 +0900

    String 최적화

---
+++ src/lang/immutable/string/LoopStringBuilderMain.java
@@ -0,0 +1,17 @@
+package lang.immutable.string;
+
+public class LoopStringBuilderMain {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        long startTime = System.currentTimeMillis();
+
+        for(int i = 0; i < 100000; i++) {
+            stringBuilder.append("Hello Java");
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(stringBuilder);
+        System.out.println(endTime - startTime);
+    }
+}
