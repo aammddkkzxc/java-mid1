17bf911cfc55373400110c329b63f68efd1450de
aammddkkzxc <ideal0427@naver.com>
2024-07-27 04:00:08 +0900

    feat: 문제와 풀이2 로또 번호 발생

---
+++ src/lang/math/test/LottoGenerator.java
@@ -0,0 +1,32 @@
+package lang.math.test;
+
+import java.util.Random;
+
+public class LottoGenerator {
+    private final Random rand = new Random();
+    private int[] lottoNumbers;
+    private int count;
+
+    public int[] generate() {
+        lottoNumbers = new int[6];
+        count = 0;
+
+        int lottoNumber = rand.nextInt(45) + 1;
+
+        if (isUnique(lottoNumber)) {
+            lottoNumbers[count] = lottoNumber;
+            count++;
+        }
+
+        return lottoNumbers;
+    }
+
+    private boolean isUnique(int number) {
+        for (int i = 0; i < count; i++) {
+            if (lottoNumbers[i] == number) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
