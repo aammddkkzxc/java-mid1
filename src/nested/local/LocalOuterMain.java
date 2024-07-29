0153355ee8ab00e27d4e7c457c0d6b3e8a5851fc
aammddkkzxc <ideal0427@naver.com>
2024-07-30 03:52:01 +0900

    feat: 지역 클래스 - 지역 변수 캡처1 지역 클래스는 인스턴스를 생성할 때 필요한 지역 변수를 먼저 캡처해서 인스턴스에 보관한다. 그리고 지역 클래스의 인스턴스를 통해 지역 변수에 접근하면, 실제로는 지역 변수에 접근하는 것이 아니라 인스턴스에 있는 캡처한 캡처 변수에 접근한다. 추가로 바깥 클래스를 참조하기 위한 필드도 확인할 수 있다 지역변수에서 변경이 일어나면?? ..더보기

--- src/nested/local/LocalOuterMain.java
+++ src/nested/local/LocalOuterMain.java
@@ -1,9 +1,17 @@
 package nested.local;
 
+import java.lang.reflect.Field;
+
 public class LocalOuterMain {
     public static void main(String[] args) {
         LocalOuterV3 local = new LocalOuterV3();
         Printer printer = local.process(3);
         printer.print();
+
+        System.out.println("필드 확인");
+        Field[] fields = printer.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println("field = " + field);
+        }
     }
 }
