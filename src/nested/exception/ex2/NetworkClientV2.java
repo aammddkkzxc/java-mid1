bff8d859a263240de96f49718a5e3d4b35ae3cbb
aammddkkzxc <ideal0427@naver.com>
2024-07-31 19:53:35 +0900

    Revert "feat:"
    This reverts commit 105d410c27a675f803031b5ef3288f62ae35ed31.

--- src/nested/exception/ex2/NetworkClientV2.java
+++ src/nested/exception/ex2/NetworkClientV2.java
@@ -19,9 +19,7 @@ public class NetworkClientV2 {
 
     public void send(String data) throws NetworkClientExceptionV2 {
         if (sendError) {
-            //throw new NetworkClientExceptionV2("sendError", address + " 서버에 데이터전송 실패: " + data);
-            //중간에 다른 예외가 발생했다고 가정
-            throw new RuntimeException("ex");
+            throw new NetworkClientExceptionV2("sendError", address + " 서버에 데이터 전송 실패: " + data);
         }
 
         System.out.println(address + " 서버에 데이터 전송: " + data);
